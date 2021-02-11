d8dc5204e17de744d5886804e4fce2b43a9f3726
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-02-11 08:59:15 +0100

    :construction: Change events target declaration

--- src/Entities/Event.ts
+++ src/Entities/Event.ts
@@ -1,6 +1,6 @@
 import Events from '../Types/Events'
 
-function Event({ type }: Events) {
+function Event(type: Events) {
 	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor, ...args: Array<any>) {
 		return Object.defineProperty(target, propertyKey, { value: { object: { module: 'EVENT', event: type, run: target[propertyKey] } } })
 	}
